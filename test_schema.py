55a54c7055300c9086b26e9058145534a5a2d249
RJ Esbenshade <rjesbenshade@gmail.com>
2026-06-05 13:31:34 -0400

    S25: Added a seperate schema file and called it in the test file

--- test_schema.py
+++ test_schema.py
@@ -2,43 +2,9 @@ import pytest
 import requests
 import jsonschema
 from jsonschema import validate
+from schemas import POST_SCHEMA, COMMENT_SCHEMA, USER_SCHEMA
 
 BASE_URL = "https://jsonplaceholder.typicode.com"
-
-# --- Define schemas ---
-POST_SCHEMA = {
-    "type": "object",
-    "required": ["id", "userId", "title", "body"],
-    "properties": {
-        "id":     {"type": "integer"},
-        "userId": {"type": "integer"},
-        "title":  {"type": "string"},
-        "body":   {"type": "string"}
-    }
-}
-
-COMMENT_SCHEMA = {
-    "type": "object",
-    "required": ["id", "postId", "name", "email", "body"],
-    "properties": {
-        "id":     {"type": "integer"},
-        "postId": {"type": "integer"},
-        "name":   {"type": "string"},
-        "email":  {"type": "string"},
-        "body":   {"type": "string"}
-    }
-}
-
-USER_SCHEMA = {
-    "type": "object",
-    "required": ["id", "name", "username", "email"],
-    "properties": {
-        "id":       {"type": "integer"},
-        "name":     {"type": "string"},
-        "username": {"type": "string"},
-        "email":    {"type": "string"}
-    }
-}
 
 # --- Schema tests ---
 
